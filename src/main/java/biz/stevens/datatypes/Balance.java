04ea67e48c29dd5d862a858ba5ae622c444173f2
Robert Stevens <git@stevens.tk>
2018-02-04 00:49:01 +0100

    started on #15

--- src/main/java/biz/stevens/datatypes/Balance.java
+++ src/main/java/biz/stevens/datatypes/Balance.java
@@ -1,5 +1,6 @@
 package biz.stevens.datatypes;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.ToString;
@@ -7,39 +8,52 @@ import lombok.ToString;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+import static biz.stevens.datatypes.FieldNames.*;
+
 @Getter
 @ToString
 public class Balance {
-    @SerializedName("CurrencyId")
+    @JsonProperty(CURRENCY_ID)
+    @SerializedName(CURRENCY_ID)
     private BigInteger currencyId;
 
-    @SerializedName("Symbol")
+    @JsonProperty(SYMBOL)
+    @SerializedName(SYMBOL)
     private String symbol;
 
-    @SerializedName("Total")
+    @JsonProperty(TOTAL)
+    @SerializedName(TOTAL)
     private BigDecimal total;
 
-    @SerializedName("Available")
-    private BigDecimal id;
+    @JsonProperty(AVAILABLE)
+    @SerializedName(AVAILABLE)
+    private BigDecimal available;
 
-    @SerializedName("Unconfirmed")
+    @JsonProperty(UNCONFIRMED)
+    @SerializedName(UNCONFIRMED)
     private BigDecimal unconfirmed;
 
-    @SerializedName("HeldForTrades")
+    @JsonProperty(HELD_FOR_TRADES)
+    @SerializedName(HELD_FOR_TRADES)
     private BigDecimal heldForTrades;
 
-    @SerializedName("PendingWithdraw")
+    @JsonProperty(PENDING_WITHDRAW)
+    @SerializedName(PENDING_WITHDRAW)
     private BigDecimal pendingWithdraw;
 
-    @SerializedName("Address")
+    @JsonProperty(ADDRESS)
+    @SerializedName(ADDRESS)
     private String address;
 
-    @SerializedName("BaseAddress")
+    @JsonProperty(BASE_ADDRESS)
+    @SerializedName(BASE_ADDRESS)
     private String baseAddress;
 
-    @SerializedName("Status")
+    @JsonProperty(STATUS)
+    @SerializedName(STATUS)
     private String status;
 
-    @SerializedName("StatusMessage")
+    @JsonProperty(STATUS_MESSAGE)
+    @SerializedName(STATUS_MESSAGE)
     private String statusMessage;
 }
---
+++ src/main/java/biz/stevens/datatypes/FieldNames.java
@@ -0,0 +1,15 @@
+package biz.stevens.datatypes;
+
+public class FieldNames {
+    public static final String CURRENCY_ID = "CurrencyId";
+    public static final String SYMBOL = "Symbol";
+    public static final String TOTAL = "Total";
+    public static final String AVAILABLE = "Available";
+    public static final String UNCONFIRMED = "Unconfirmed";
+    public static final String HELD_FOR_TRADES = "HeldForTrades";
+    public static final String PENDING_WITHDRAW = "PendingWithdraw";
+    public static final String ADDRESS = "Address";
+    public static final String BASE_ADDRESS = "BaseAddress";
+    public static final String STATUS = "Status";
+    public static final String STATUS_MESSAGE = "StatusMessage";
+}
